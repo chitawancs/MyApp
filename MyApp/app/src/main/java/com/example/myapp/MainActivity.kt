5af6daf2947aeb472b7c2a17bb8a60508a9412ce
YENTEDKUB\Chitawan <chitawan.cs@gmail.com>
2021-03-24 22:40:27 +0700

    - format

--- MyApp/app/src/main/java/com/example/myapp/MainActivity.kt
+++ MyApp/app/src/main/java/com/example/myapp/MainActivity.kt
@@ -5,20 +5,12 @@ import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.myapp.databinding.ActivityMainBinding
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 
 private const val LOG_TAG_YENTED = "YENTED"
 
 class MainActivity : AppCompatActivity() {
 
     private var binding: ActivityMainBinding? = null
-
-    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,9 +21,7 @@ class MainActivity : AppCompatActivity() {
         binding?.executePendingBindings()
 
         binding?.viewModel?.timer?.observe(this, {
-            Log.d("LOG_TAG_YENTED", it)
+            Log.d(LOG_TAG_YENTED, it)
         })
-
-
     }
 }

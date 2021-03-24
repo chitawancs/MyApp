69a577999c4f697b185c6183c211f66f0e35ef6a
Chitawan Duangsai <chitawan.cs@gmail.com>
2021-03-24 22:14:49 +0700

    - add log debug

--- MyApp/app/src/main/java/com/example/myapp/MainActivity.kt
+++ MyApp/app/src/main/java/com/example/myapp/MainActivity.kt
@@ -12,23 +12,24 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
+private const val LOG_TAG_YENTED = "LOG_TAG_YENTED"
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private var binding : ActivityMainBinding? = null
+    private var binding: ActivityMainBinding? = null
 
     private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding?.viewModel = MainViewModel()
         binding?.lifecycleOwner = this
         binding?.executePendingBindings()
 
-        binding?.viewModel?.timer?.observe(this,{
-            Log.d("yented", it)
+        binding?.viewModel?.timer?.observe(this, {
+            Log.d("LOG_TAG_YENTED", it)
         })
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
